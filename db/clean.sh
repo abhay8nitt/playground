0aab047ac2dc579fdfd05c77e6db62bbcda10fcf
amit.saha <amit.saha@fireeye.com>
2019-07-21 21:07:05 +0530

    DB seed script to create database tables

--- db/clean.sh
+++ db/clean.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# create database
+mysql --defaults-file='.my.cnf' --defaults-group-suffix='_root' \
+-ve "DROP DATABASE IF EXISTS project"
+
+# create database
+mysql --defaults-file='.my.cnf' --defaults-group-suffix='_root' \
+-ve "DROP USER IF EXISTS 'admin'"
+
 sudo apt-get purge mysql*
 sudo apt-get autoremove
 sudo apt-get autoclean
